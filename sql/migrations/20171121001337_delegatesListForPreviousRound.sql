c67c475e384b5bc28a9f9516b020ebded2f57a30
Mariusz Serek <mariusz@serek.net>
2017-11-21 17:30:58 +0100

    Add trigger for snapshoting delegates before round ends

---
+++ sql/migrations/20171121001337_delegatesListForPreviousRound.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+-- Create function for performing delegates snapshot
+CREATE FUNCTION perform_delegates_snapshot() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+	BEGIN
+	    EXECUTE 'DROP TABLE IF EXISTS delegates_snapshot';
+        EXECUTE 'CREATE TABLE delegates_snapshot AS SELECT address, pk, voters_balance FROM delegates';
+	RETURN NEW;
+END $$;
+
+-- Create trigger that will execute 'perform_delegates_snapshot' before insertion of last block of round
+CREATE TRIGGER perform_delegates_snapshot
+	BEFORE INSERT ON blocks
+	FOR EACH ROW
+	WHEN (NEW.height % 101 = 0 OR NEW.height = 1)
+	EXECUTE PROCEDURE perform_delegates_snapshot();
+
+COMMIT;
